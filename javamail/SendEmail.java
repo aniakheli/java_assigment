a7647f2ad5abc8e99fddc6c817ea3bffe161a38a
aniakheli <60497215+aniakheli@users.noreply.github.com>
2020-07-14 18:16:19 +0545

    Create SendEmail.java

---
+++ javamail/SendEmail.java
@@ -0,0 +1,32 @@
+import java.util.*;  
+import javax.mail.*;  
+import javax.mail.internet.*;  
+import javax.activation.*;  
+  
+public class SendEmail  
+{  
+ public static void main(String [] args){  
+      String to = "sachcollege2073@gmail.com"; 
+      String from = "chhetry.ani175@gmail.com"; 
+      String host = "localhost";
+  
+     
+      Properties properties = System.getProperties();  
+      properties.setProperty("mail.smtp.host", host);  
+      Session session = Session.getDefaultInstance(properties);  
+  
+     
+      try{  
+         MimeMessage message = new MimeMessage(session);  
+         message.setFrom(new InternetAddress(from));  
+         message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
+         message.setSubject("Ping");  
+         message.setText("Hello, this is example of sending email  ");  
+  
+         
+         Transport.send(message);  
+         System.out.println("message sent successfully....");  
+  
+      }catch (MessagingException mex) {mex.printStackTrace();}  
+   }  
+} 
